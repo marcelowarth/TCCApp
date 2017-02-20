a0a26b8a717b46be02bbd9c68e7deffb10d06372
Unknown <marcelowarth@gmail.com>
2017-02-20 08:32:34 -0300

    Criação do projeto, placeholders de splash e main menu

---
+++ App/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float loadNextLevelAfter = 2.5f;
+	
+	void Start () {
+		if(SceneManager.GetActiveScene().buildIndex == 0) {
+			Invoke ("LoadNextLevel", loadNextLevelAfter);
+		} else if(loadNextLevelAfter <= 0f) {
+			Debug.Log("Use positive number on Load Next Level After!");
+		}
+	}
+	
+	public void LoadLevel(string name) {
+        SceneManager.LoadScene(name);
+	}
+	
+	public void QuitRequest() {
+		Application.Quit();
+	}
+	
+	public void LoadNextScene() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
